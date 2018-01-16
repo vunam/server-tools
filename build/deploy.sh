620b475e28b7398b2060abdb5fe9087dd3387e71
Vu Nam Nguyen <vunam281@gmail.com>
2018-01-16 18:54:50 +0000

    Revert docker volume

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,7 +3,7 @@ ssh -v circleci@136.144.176.117 << EOF
 
 echo '1. Updating sources'
 cd /server/tools/
-git clean -fdx --exclude=".env" --exclude=".env.grafana"
+git clean -fdx --exclude=".env" --exclude=".env.grafana"  --exclude="grafana_data"  --exclude="prometheus_data"
 git fetch
 git checkout --force master
 git pull origin master
